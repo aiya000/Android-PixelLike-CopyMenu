9cd4f6a1651be5864a081ec660c81fee69f954cc
aiya000 <aiya000.develop@gmail.com>
2026-09-12 11:47:59 +0900

    chore: Bump the version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "io.github.aiya000.pixellikecopymenu"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     signingConfigs {
